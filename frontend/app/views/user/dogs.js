6eb9d6167c7516df3fb11a2eca9665b221818ea2
David Lai <me@dlai0001.com>
2014-11-22 15:00:42 -0800

    swipe gestures

--- frontend/app/views/user/dogs.js
+++ frontend/app/views/user/dogs.js
@@ -1,28 +1,52 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
+    didInsertElement: function () {
+        "use strict";
+
+        this._initSwipeGestures();
+    },
+
+    _initSwipeGestures: function() {
+        "use strict";
+        var mc = Hammer(this.$()[0]);
+        mc.on("swipeleft", function (evt) {
+            console.log("swipe left detected", evt);
+            this.send("like");
+
+            return false;
+        }.bind(this));
+
+        mc.on("swiperight", function (evt) {
+            console.log("swipe rigth detected", evt);
+            this.send("dislike");
+            
+            return false;
+        }.bind(this));
+    },
+
     actions: {
-        like: function() {
+        like: function () {
             "use strict";
             console.log("like");
 
-            this.$("img").animate({left:"-2500px", top:"-30px"},
-            500,
-            function() {
-                this.controller.send("like");
-                this.$("img").css({left:"0px", top:"0px"});
-            }.bind(this));
+            this.$("img").animate({left: "-2500px", top: "-30px"},
+                500,
+                function () {
+                    this.controller.send("like");
+                    this.$("img").css({left: "0px", top: "0px"});
+                }.bind(this));
         },
 
-        dislike: function() {
+        dislike: function () {
             "use strict";
             console.log("dislike");
 
-            this.$("img").animate({left:"2500px", top:"-30px"},
+            this.$("img").animate({left: "2500px", top: "-30px"},
                 500,
-                function() {
+                function () {
                     this.controller.send("dislike");
-                    this.$("img").css({left:"0px", top:"0px"});
+                    this.$("img").css({left: "0px", top: "0px"});
                 }.bind(this));
         }
     }
