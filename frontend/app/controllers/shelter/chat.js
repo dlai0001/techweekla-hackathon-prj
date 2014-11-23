607c668e42a59e907ef1005dea09fcf5b84591bc
Artyom Keydunov <artyom.keydunov@gmail.com>
2014-11-22 16:51:05 -0800

    Style chat

--- frontend/app/controllers/shelter/chat.js
+++ frontend/app/controllers/shelter/chat.js
@@ -1,5 +1,28 @@
 import Ember from 'ember';
 
+Ember.MessagesArray = Ember.ArrayProxy.extend({
+  init: function() {
+    var messages = Ember.A();
+    this.set("content", messages);
+
+    this.socket.on("message", function(data) {
+      messages.pushObject(data.message);
+    });
+
+    this._super();
+  },
+
+  replaceContent: function(ids, amt, object) {
+   this.socket.emit("send", { message: object });
+  }
+});
+
 export default Ember.Controller.extend({
-    _sinch: null,
+  msg: "",
+  actions: {
+    sendMessage: function() {
+      this.pushObject(this.get("msg"));
+      this.set("msg", "");
+    }
+  }
 });
